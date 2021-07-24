459db0daa0cceab73a2de3bd7eb890f6fbad42b8
ayush893 <mauryaayush893@gmail.com>
2021-07-24 23:10:41 +0530

    doing test

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,15 @@ class PagesController < ApplicationController
 		@title = 'About us';
 		@content = 'This is the first blog page';
 	end
+scope :recents, where("created_at > ?", Time.now-7.days)
 
+    def self.get_student_attendance_details
+        attendance = Attendances.recents
+        dict = { }
+        for student in attendance
+            if dict.include?(student.student_id):
+                dict[student.student_id] =  dict[student.student_id].push(student)
+            else
+                d[student.student_id] = [student]
+    end
 end
